c3812fddf93b2914487e6493bf95ac4b1bf50662
omochi <chocochi003@outlook.jp>
2021-05-10 07:08:43 +0000

    使用していないファイルの削除

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,6 +3,6 @@ class Micropost < ApplicationRecord
   
   validates :content, presence: true, length: {maximum: 255}
   
-  has_many :reverses_of_favorite, class_name: 'Favorite'
-  has_many :fav_users, through: :reverses_of_favorite, source: :user
+  has_many :favorites
+  has_many :fav_users, through: :favorites, source: :user
 end
